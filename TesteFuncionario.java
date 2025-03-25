14d4b2cc924cd85732fe1ff7bf6be6deed32c19a
EloaLorena <eloalorena1980@gmail.com>
2025-03-25 15:33:20 -0300

    commit

--- TesteFuncionario.java
+++ TesteFuncionario.java
@@ -1,12 +1,14 @@
 // Classe TesteFuncionario para executar o código
 public class TesteFuncionario {
     public static void main(String[] args) {
-        Funcionario funcionario1 = new Funcionario("Carlos", 'J', 5000, false);
-        funcionario1.exibirDados(true, true); // Exemplo com falta e hora extra
 
+        // Exemplo com falta e hora extra
+        Funcionario funcionario1 = new Funcionario("Carlos", 'J', 5000, false);
+        funcionario1.exibirDados(true, true); 
         System.out.println();
 
+        // Exemplo de funcionário afastado
         Funcionario funcionario2 = new Funcionario("Ana", 'S', 8000, true);
-        funcionario2.exibirDados(false, false); // Exemplo de funcionário afastado
+        funcionario2.exibirDados(false, false); 
     }
 }
